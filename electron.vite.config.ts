bcb09e5477a5f14b5662ebcb49dbad771142a71b
lifangzheng <1397118980@qq.com>
2026-06-25 22:51:57 +0800

    init

---
+++ electron.vite.config.ts
@@ -0,0 +1,18 @@
+import { resolve } from 'path'
+import { defineConfig } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {},
+  preload: {},
+  renderer: {
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@common': resolve('src/common'),
+        '@types': resolve('src/types')
+      }
+    },
+    plugins: [react()]
+  }
+})
---
+++ src/main/index.ts
@@ -0,0 +1,73 @@
+import { app, shell, BrowserWindow, ipcMain } from 'electron'
+import { join } from 'path'
+import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import icon from '../../resources/icon.png?asset'
+
+function createWindow(): void {
+  // 创建浏览器窗口。
+  const mainWindow = new BrowserWindow({
+    width: 900,
+    height: 670,
+    show: false,
+    autoHideMenuBar: true,
+    ...(process.platform === 'linux' ? { icon } : {}),
+    webPreferences: {
+      preload: join(__dirname, '../preload/index.js'),
+      sandbox: false
+    }
+  })
+
+  mainWindow.on('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  mainWindow.webContents.setWindowOpenHandler((details) => {
+    shell.openExternal(details.url)
+    return { action: 'deny' }
+  })
+
+  // 基于 electron-vite cli 的渲染进程 HMR。
+  // 开发环境加载远程 URL，生产环境加载本地 HTML 文件。
+  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
+    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  } else {
+    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+  }
+}
+
+// 当 Electron 完成初始化并准备创建浏览器窗口时，将调用此方法。
+// 某些 API 只能在此事件发生后使用。
+app.whenReady().then(() => {
+  // 为 Windows 设置应用用户模型 ID
+  electronApp.setAppUserModelId('com.electron')
+
+  // 开发环境默认通过 F12 打开或关闭 DevTools
+  // 生产环境忽略 CommandOrControl + R。
+  // 参见 https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+  app.on('browser-window-created', (_, window) => {
+    optimizer.watchWindowShortcuts(window)
+  })
+
+  // IPC 测试
+  ipcMain.on('ping', () => console.log('pong'))
+
+  createWindow()
+
+  app.on('activate', function () {
+    // 在 macOS 上，当点击 dock 图标且没有其他窗口打开时，
+    // 通常会重新创建一个窗口。
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// 当所有窗口关闭时退出应用，macOS 除外。在 macOS 上，
+// 应用程序及其菜单栏通常会保持活跃，直到用户
+// 通过 Cmd + Q 显式退出。
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+// 在此文件中，你可以包含应用主进程的其余特定代码。
+// 你也可以将它们放在单独的文件中并在此处引入。
